1c35a3fb67074c15378a110cba31327a6bf88a13
Benitonz <benzoni2009@gmail.com>
2025-09-19 16:10:34 +0200

    feat: init cv downloader

---
+++ src/app/cv-download/cv-download.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cv-download',
+  templateUrl: './cv-download.component.html',
+  styleUrls: ['./cv-download.component.scss']
+})
+export class CvDownloadComponent {}
